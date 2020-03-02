c44597a3cf7a36f032f6d5cf328a56694d133dda
Gian <gsartor@madiba.attlocal.net>
2020-03-02 11:46:02 -0500

    Update connections.tf

--- connections.tf
+++ connections.tf
@@ -9,9 +9,9 @@ variable "ssh_public_key" {}
 variable "ssh_private_key" {}
 
 provider "oci" {
-  tenancy_ocid     = "${var.tenancy_ocid}"
-  user_ocid        = "${var.user_ocid}"
-  fingerprint      = "${var.fingerprint}"
-  private_key_path = "${var.private_key_path}"
-  region           = "${var.region}"
+  tenancy_ocid     = "var.tenancy_ocid"
+  user_ocid        = "var.user_ocid"
+  fingerprint      = "var.fingerprint"
+  private_key_path = "var.private_key_path"
+  region           = "var.region"
 }
