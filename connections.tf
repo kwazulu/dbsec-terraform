1e103f3e916f57e89f3a176f870fc2103ad2dc61
Gian <gsartor@madiba.attlocal.net>
2020-03-03 16:34:02 -0500

    trying to fix network creation

--- connections.tf
+++ connections.tf
@@ -7,6 +7,7 @@ variable "region" {}
 variable "compartment_ocid" {}
 variable "ssh_public_key" {}
 variable "ssh_private_key" {}
+## variable "bucket_name" {}
 
 provider "oci" {
   version          = ">= 3.0.0"
@@ -17,10 +18,19 @@ provider "oci" {
   region           = "${var.region}"
 }
 
-variable shape_DBSec_Lab{ default = "VM.Standard2.4" }
-variable shape_AV12cR2_bp11{ default = "VM.Standard2.2" }
-variable shape_OKV_18cR2{ default = "VM.Standard2.2" }
-variable shape_DBF_12cR2_bp11{ default = "VM.Standard2.2" }
+variable avs_object_name{ default = "Template_AV-12cR2-bp11" }
+variable dbf_object_name{ default = "Template_DBF-12cR2-bp11" }
+variable okv_object_name{ default = "Template_OKV-18cR2" }
+variable dbl_object_name{ default = "Template_DBSec-Lab" }
 
-variable bucketname
-variable O=objectname
+variable shape_DBSec_Lab{ default = "VM.Standard2.4" }
+variable shape_AVServer{ default = "VM.Standard2.2" }
+variable shape_OKVServer{ default = "VM.Standard2.2" }
+variable shape_DBFServer{ default = "VM.Standard2.2" }
+
+variable availability_domain_number{ default = "2" }
+
+variable okv_ocid{ default = "ocid1.image.oc1.iad.aaaaaaaap7rdlp5skbstovzv5ojrl4zxzr5dm72eecufpchjgse36wfgywja" }
+variable avs_ocid{ default = "ocid1.image.oc1.iad.aaaaaaaacqf6yjfz22zdyf47f2oako72clmq25d4ghot4s7ndu5jrqjhdjba" }
+variable dbf_ocid{ default = "ocid1.image.oc1.iad.aaaaaaaag2zbibwzjcrj3v2rxlhhml4fkcn3laoufk5ps46ps4otiomtosaa" }
+variable dbl_ocid{ default = "ocid1.image.oc1.iad.aaaaaaaah7ba2y5x5sdx3uj7txl6vunpmupqvas67lqrpbhtnv4hmq2w77qq" }
--- network.tf
+++ network.tf
@@ -1,56 +1,37 @@
 resource "oci_core_vcn" "DBSec_VCN_tf" {
   cidr_block     = "10.0.0.0/16"
-  compartment_id = "${var.compartment_ocid}"
-  defined_tags   = {}
-
-  display_name = "DBSec_VCN_tf"
   dns_label    = "dbsecvcn"
+  compartment_id = "${var.compartment_ocid}"
+  display_name = "DBSec_VCN_tf"
 }
 
 resource "oci_core_dhcp_options" "Default-DHCP-Options-for-DBSec_VCN_tf" {
-  defined_tags = {}
-  
   vcn_id = "${oci_core_vcn.DBSec_VCN_tf.id}"
   compartment_id = "${var.compartment_ocid}"
   display_name = "Default DHCP Options for DBSec_VCN_tf"
 
-  ## manage_default_resource_id = "${oci_core_vcn.DBSec_VCN_tf.default_dhcp_options_id}"
-
   options {
-    custom_dns_servers = []
-
-    server_type = "VcnLocalPlusInternet"
     type        = "DomainNameServer"
+    server_type = "VcnLocalPlusInternet"
   }
 
   options {
-    search_domain_names = [
-      "dbsecvcn.oraclevcn.com",
-    ]
-
     type = "SearchDomain"
+    search_domain_names = ["dbsecvcn.oraclevcn.com"]
   }
 }
 
 resource "oci_core_internet_gateway" "Internet-Gateway-DBSec_VCN_tf" {
   compartment_id = "${var.compartment_ocid}"
-  defined_tags   = {}
-
-  display_name = "Internet Gateway-DBSec_VCN_tf"
+  display_name = "Internet-Gateway-DBSec_VCN_tf"
   enabled      = "true"
-
   vcn_id = "${oci_core_vcn.DBSec_VCN_tf.id}"
 }
 
 resource "oci_core_route_table" "Default-Route-Table-for-DBSec_VCN_tf" {
-  defined_tags = {}
-  
-  vcn_id = "${oci_core_vcn.DBSec_VCN_tf.id}"
   compartment_id = "${var.compartment_ocid}"
-
-  display_name = "Default Route Table for DBSec_VCN_tf"
-
-  ## manage_default_resource_id = "${oci_core_vcn.DBSec_VCN_tf.default_route_table_id}"
+  vcn_id = "${oci_core_vcn.DBSec_VCN_tf.id}"
+  display_name = "Default-Route-Table-for-DBSec_VCN_tf"
 
   route_rules {
     destination       = "0.0.0.0/0"
@@ -59,48 +40,61 @@ resource "oci_core_route_table" "Default-Route-Table-for-DBSec_VCN_tf" {
   }
 }
 
-
 resource "oci_core_security_list" "Default-Security-List-for-DBSec_VCN_tf" {
-  defined_tags = {}
-
   vcn_id = "${oci_core_vcn.DBSec_VCN_tf.id}"
   compartment_id = "${var.compartment_ocid}"  
+  display_name = "Default-Security-List-for-DBSec_VCN_tf"
 
-  display_name = "Default Security List for DBSec_VCN_tf"
-
+// allow outbound tcp traffic on all ports
   egress_security_rules {
-    destination      = "0.0.0.0/0"
-    destination_type = "CIDR_BLOCK"
-
-    protocol  = "all"
-    stateless = "false"
+    destination = "0.0.0.0/0"
+    protocol    = "6"
   }
 
+  // allow outbound udp traffic on a port range
+  egress_security_rules {
+    destination = "0.0.0.0/0"
+    protocol    = "17"        
+    stateless   = true
+
+    udp_options {
+      min = 319
+      max = 320
+    }
+  }
+
+  // allow inbound ssh traffic
   ingress_security_rules {
-    protocol = "all"
+    protocol  = "6"         
+    source    = "0.0.0.0/0"
+    stateless = false
 
-    source      = "10.0.0.0/16"
-    source_type = "CIDR_BLOCK"
-    stateless   = "false"
+    tcp_options {
+      min = 22
+      max = 22
+    }
   }
 
+  // allow inbound icmp traffic of a specific type
+  ingress_security_rules {
+    protocol  = 1
+    source    = "0.0.0.0/0"
+    stateless = true
+
+    icmp_options {
+      type = 3
+      code = 4
+    }
+  }
 }
 
 resource "oci_core_subnet" "dbsecsubnet" {
-  cidr_block     = "10.0.0.0/24"
-  compartment_id = "${var.compartment_ocid}"
-  defined_tags   = {}
-
-  dhcp_options_id = "${oci_core_vcn.DBSec_VCN_tf.default_dhcp_options_id}"
-  display_name    = "dbsecsubnet"
-  dns_label       = "dbsecsubnet"
-
-  prohibit_public_ip_on_vnic = "false"
-  route_table_id             = "${oci_core_vcn.DBSec_VCN_tf.default_route_table_id}"
-
-  security_list_ids = [
-    "${oci_core_vcn.DBSec_VCN_tf.default_security_list_id}",
-  ]
-
-  vcn_id = "${oci_core_vcn.DBSec_VCN_tf.id}"
+  cidr_block        = "10.0.0.0/24"
+  display_name      = "dbsecsubnet"
+  dns_label         = "dbsecsubnet"
+  compartment_id    = "${var.compartment_ocid}"
+  vcn_id            = "${oci_core_vcn.DBSec_VCN_tf.id}"
+  security_list_ids = ["${oci_core_vcn.DBSec_VCN_tf.default_security_list_id}"]
+  route_table_id    = "${oci_core_vcn.DBSec_VCN_tf.default_route_table_id}"
+  dhcp_options_id   = "${oci_core_vcn.DBSec_VCN_tf.default_dhcp_options_id}"
 }
